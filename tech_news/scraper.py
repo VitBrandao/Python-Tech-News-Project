4afca7cfd0f4e930f76136a80902400d76fc2e9f
vitor <vitorsbsilva@gmail.com>
2022-08-23 15:19:13 -0300

    refactor: fetch() in scraper.py. move finally operations to try

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -13,34 +13,34 @@ def fetch(url):
             timeout=3
         )
         time.sleep(1)
-    except requests.ReadTimeout:
-        return None
 
-    finally:
         if(response.status_code != 200):
             return None
 
         return response.text
 
+    except requests.ReadTimeout:
+        return None
+
 
 # fetch(trybe_url)
 
 
-# # Requisito 2
-# def scrape_novidades(html_content):
-#     """Seu código deve vir aqui"""
+# Requisito 2
+def scrape_novidades(html_content):
+    return 0
 
 
-# # Requisito 3
-# def scrape_next_page_link(html_content):
-#     """Seu código deve vir aqui"""
+# Requisito 3
+def scrape_next_page_link(html_content):
+    return 0
 
 
-# # Requisito 4
-# def scrape_noticia(html_content):
-#     """Seu código deve vir aqui"""
+# Requisito 4
+def scrape_noticia(html_content):
+    return 0
 
 
-# # Requisito 5
-# def get_tech_news(amount):
-#     """Seu código deve vir aqui"""
+# Requisito 5
+def get_tech_news(amount):
+    return 0
