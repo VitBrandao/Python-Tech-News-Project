59884ef626ccc693849fc40ff752ccafd1c27fea
vitor <vitorsbsilva@gmail.com>
2022-08-23 14:56:02 -0300

    feat: fetch() in scraper.py first test

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,46 @@
-# Requisito 1 - scrapper
+import requests
+import time
+
+# trybe_url = "https://blog.betrybe.com"
+
+
+# Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=3
+        )
+        time.sleep(1)
+    except requests.ReadTimeout:
+        return None
+
+    finally:
+        if(response.status_code != 200):
+            return None
+
+        return response.text
 
 
-# Requisito 2
-def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+# fetch(trybe_url)
 
 
-# Requisito 3
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+# # Requisito 2
+# def scrape_novidades(html_content):
+#     """Seu código deve vir aqui"""
 
 
-# Requisito 4
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+# # Requisito 3
+# def scrape_next_page_link(html_content):
+#     """Seu código deve vir aqui"""
 
 
-# Requisito 5
-def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+# # Requisito 4
+# def scrape_noticia(html_content):
+#     """Seu código deve vir aqui"""
+
+
+# # Requisito 5
+# def get_tech_news(amount):
+#     """Seu código deve vir aqui"""
